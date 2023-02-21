2eb11090cc5e69a5cf071a89d3eab4a369bb8f56
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-21 09:57:40 -0800

    Current Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 	char *c = "_putchar";
 	int i;
 
-	for (i = 0; i <=8; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		_putchar(c[i]);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 
 	return (0);
 }
