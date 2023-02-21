b432a49d31b181a5de59cceb7c62c0b7e22d91e5
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-21 12:23:53 -0800

    Current Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - For printing sign absolute
+ * *@n: int for the argument of function
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+		return (n);
+	}
+		else
+		{
+			return (n * -1);
+		}
+
+}
