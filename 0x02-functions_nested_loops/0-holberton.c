153b29ce201ad6a4c6e9bba2d167efad1615d126
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-21 04:14:12 -0800

    Current Commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Intro to the main program
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	char *c = "_putchar";	
+        int i;
+        for(i = 0; i < 9; i++) {
+                _putchar(c[i]);
+        }
+        _putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - For printing the alphabet 10 times
+ *
+ *no return since its void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+}
