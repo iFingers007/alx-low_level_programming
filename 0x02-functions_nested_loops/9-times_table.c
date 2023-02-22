552c91120831c9feb9048e1d72cdc937414ab0c0
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-21 17:01:13 -0800

    Current Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,20 +9,21 @@
 
 void times_table(void)
 {
-int num, mul, pro;
-
-for (num = 0; num <= 9; num++)
-{
-	_putchar('0');
-	for (mul = 1; mul <= 9; mul++)
-		_putchar(',');
-	_putchar(' ');
-	pro = num * mul;
-	if (pro <= 9)
-		_putchar(' ');
-	else
-		_putchar((pro / 10) + '0');
-	_putchar((pro % 10) + '0');
-}
-_putchar('\n');
+	int num, mult, prod;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
