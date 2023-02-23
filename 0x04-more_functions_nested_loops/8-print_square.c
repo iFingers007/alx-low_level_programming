d715ad7613c8ff7e0b02a4cb26b75ca2f7deec30
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-23 10:38:44 -0800

    Current Commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: size of square parameter
+ */
+
+void print_square(int size)
+{
+int i = 1;
+while (i <= size)
+{
+_putchar(35);
+i++;
+}
+_putchar('\n');
+}
