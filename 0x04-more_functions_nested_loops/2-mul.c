9c0ccb6f77174c2203bdf889e336864dcf435456
Abu Jehoiada <abujehoiada@gmail.com>
2023-02-23 08:38:13 -0800

    Current Commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - For multiplying two integers
+ * *@a: First integer
+ * *@b: Second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
