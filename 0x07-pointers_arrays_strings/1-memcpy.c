3fab79716bcf63fbedc627709ffd1525baf70e69
Abu Jehoiada <abujehoiada@gmail.com>
2023-03-06 13:24:02 -0800

    Current Commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,26 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _memcpy - Personal Memset
  *@src: Source of memory area
- *@dst: Destination memory area
+ *@dest: Destination memory area
  *@n: Bytes to be copied
  *Return: Pointer to the memory area dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *pszDest = (char *)dst;
-	char *pszSource = ( char*)src;
+	char *pszDest = (char *)dest;
+	char *pszSource = (char *)src;
 
 	if ((pszDest != NULL) && (pszSource != NULL))
 	{
-		while(n)
+		while (n)
 		{
-			*(pszDest++)= *(pszSource++);
-			--cnt;
+			*(pszDest++) = *(pszSource++);
+			--n;
 		}
 	}
-	return dst;
+	return (dest);
 }
