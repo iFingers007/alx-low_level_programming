fe25a8a3e8f9efadfea0d56f64bcea7cb6ff9609
Abu Jehoiada <abujehoiada@gmail.com>
2023-03-07 08:34:37 -0800

    Current Commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the factorial of an integer
+ * @x: first integer
+ * @y: second integer
+ * Return: power of the result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+		else if (y == 0)
+		{
+			return (1);
+		}
+	else
+	{
+		return (x * _pow_recursion (x, y - 1));
+	}
+}
