32371a991eba13933dc64ac854caca451a5a3d91
Abu Jehoiada <abujehoiada@gmail.com>
2023-03-07 07:51:08 -0800

    Current Commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - prints the factorial of an integer
+ * @n: integer
+ * Return: factorial of the number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
