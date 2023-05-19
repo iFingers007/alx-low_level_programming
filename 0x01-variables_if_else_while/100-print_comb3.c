d30e6d798d859ba184537bc44ea87e015f9f5f84
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-19 15:17:06 -0700

    Updated

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ *Return: Always 0 at success
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(48 + i);
+				putchar(48 + j);
+				{
+						if (i < 8)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
