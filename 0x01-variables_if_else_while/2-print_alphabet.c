649d3de547cebaf1a731ed81a027e50722983863
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-19 06:28:14 -0700

    Updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ *Return: Always 0 at success
+*/
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
