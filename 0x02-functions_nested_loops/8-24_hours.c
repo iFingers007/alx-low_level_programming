ec162eb047f62cb59011ed0d82076874552c5bb9
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-23 15:17:24 -0700

    Updated

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *void jack_bauer(void)
+ *
+ *
+ *Return: Void
+*/
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
