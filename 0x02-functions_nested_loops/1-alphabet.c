26767a4fa44440c1bf7a7ed21bed1e93356e455f
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-23 12:08:57 -0700

    Updated

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Prints the alphabet
+ *
+ *Return: Void
+*/
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; i++)
+		_putchar(a);
+	_putchar('\n');
+}
