11017cfc5b2fae665d261b63317b38eb366a7464
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-23 18:28:50 -0700

    Updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,5 +12,21 @@ void times_table(void)
 	int n, m, r;
 
 	for (n = 0; n <= 9; n++)
-		_putchar
+	{
+		_putchar('0');
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			r = n * m;
+
+			if (r <= 9)
+				_putchar(' ');
+			else
+				_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
