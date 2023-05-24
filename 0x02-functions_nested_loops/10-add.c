bac663d56e1f9fdaf046bf213024da83d37a995c
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-23 17:23:09 -0700

    Updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - Sums two integers
+ *@i: First integer
+ *@d: Second Integer
+ *
+ *Return: Value of integer
+*/
+
+int add(int i, int d)
+{
+	int res = (i + d);
+
+	return (res);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *times_table - Prints the multiplication table
+ *
+ *
+ *Return: void
+*/
+
+void times_table(void)
+{
+	int n, m, r;
+
+	for (n = 0; n <= 9; n++)
+		_putchar
+}
