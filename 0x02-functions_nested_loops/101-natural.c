fa7ec0bf2f69c3671793773b50aea357a5a2ae92
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-24 17:59:36 -0700

    New Update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *
+ *Return: Always Zero at success
+*/
+
+int main (void)
+{
+	int i, r;
+
+	for (i =1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+	{
+		r += i;
+	}
+	}
+	printf("Sum is : %d\n", r);
+
+	return (0);
+}
