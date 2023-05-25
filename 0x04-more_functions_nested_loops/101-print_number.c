5c02f3c38ac0abc567bd8348118def905d1f161a
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-25 09:28:58 -0700

    Updated

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_number - Prints number
+ *@n: Integer number given
+ *
+ *Return:void
+*/
+void print_number(int n)
+{
+	unsigned int i, j, count;
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * - 1;
+	}
+	else
+		i = n;
+	j = i;
+	count = 1;
+	while(j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+		_putchar(((i / count) % 10) + 48);
+}
