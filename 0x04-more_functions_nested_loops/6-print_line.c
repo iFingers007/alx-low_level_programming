f5ed3503feebb9ebd43f46808016ffbcc4243411
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-25 06:07:38 -0700

    Updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_line - Prints line in terminal
+ *@n: Number of times character appears
+ *
+ *Return: void
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
