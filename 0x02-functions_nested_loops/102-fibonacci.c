781c215f250a5992ebbd6b75aab06aae63a341bb
Abu Jehoiada <abujehoiada@gmail.com>
2023-05-24 18:38:56 -0700

    New Update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Entry Point of program
+ *
+ *
+ *Return: Zero as success
+*/
+
+int main(void)
+{
+	int i, fn, sn, n;
+
+	fn = 1;
+	sn = 2;
+
+	printf("%d, %d, ", fn, sn);
+	for (i = 0; i < 48; i++)
+	{
+		n = fn +sn;
+		printf("%d, ", sn);
+		fn = sn;
+		sn = n;
+	}
+	printf("\n");
+
+	return (0);
+}
