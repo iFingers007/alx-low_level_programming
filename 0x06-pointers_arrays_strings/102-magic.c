1542942391d514957dab4a61aaed6c80f8d21bf0
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-01 17:33:42 -0700

    Updated

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *main - Entry into the program
+ *main - Ebtry to program
  *
  *
- *Return: Zero on success
+ *Return: Zero on succcess
 */
+
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -24,9 +24,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	p[5] = 98;
+ p[5] =98;
   /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-
-	return (0);
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
