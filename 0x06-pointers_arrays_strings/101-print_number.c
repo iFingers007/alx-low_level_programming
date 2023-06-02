17481bf48e34f699cc5ed3adbfc5809c42da5b96
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-01 18:44:23 -0700

    Updated

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,11 +20,11 @@ void print_number(int n)
 		m = n;
 	d = m;
 	count = 1;
-	while (d < 9)
+	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
 	}
 	for (; count >= 1; count /= 10)
-		_putchar(((m / count) % 10) + 48)
+		_putchar(((m / count) % 10) + 48);
 }
