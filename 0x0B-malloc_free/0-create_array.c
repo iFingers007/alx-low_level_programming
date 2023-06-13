7d9521c5c59b2e34837d1163f58aa2ca71d2a22e
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-13 08:21:12 -0700

    Updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s =(char *) malloc(sizeof(char) * size);
+	s = (char *) malloc(sizeof(char) * size);
 
 	if (s == NULL)
 		return (NULL);
