fe339a24ab5a16b99d61b55fe51739ac6fcd440a
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-13 21:12:48 -0700

    Updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,49 +2,40 @@
 #include <stdlib.h>
 
 /**
- *str_concat - Concatenates two strings
+ *str_concat - Concat two strings
  *@s1: Destination string
  *@s2: Source string
  *
- *Return: concatenated string or NULL at failure
+ *Return: Concatenated string
 */
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t s1_len = 0, s2_len = 0, concatlen = 0;
-	char *temp, *result, *p;
+	size_t s1_l = 0, s2_l = 0, con_len = 0;
+	char *temp, *p, *result;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	temp = s1;
-	while (*temp != '\0')
-	{
-		s1_len++;
-		temp++;
-	}
-	temp = s2;
-	while (*temp != '\0')
-	{
-		s2_len++;
-		temp++;
-	}
-	concatlen = s1_len + s2_len;
-	result = (char *)malloc(sizeof(char) * (concatlen + 1));
-
+	for (temp = s1; *temp != '\0'; temp++)
+		s1_l++;
+	for (temp = s2; *temp != '\0'; temp++)
+		s2_l++;
+	con_len = s1_l + s2_l;
+	result = malloc((con_len + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
-	p = result;
-	while (*s1 != '\0')
+	for (p = result; *s1 != '\0'; s1++)
 	{
 		*p = *s1;
 		p++;
-		s++;
 	}
-	for (p = result; *s2 != '\0'; s++)
+	for (; *s2 != '\0'; s2++)
+	{
+		*p = *s2;
 		p++;
+	}
 	*p = '\0';
-
 	return (result);
 }
