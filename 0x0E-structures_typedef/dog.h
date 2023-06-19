5750bec1f70745b340748f1d3303af71a7bfb606
Abu Jehoiada <abujehoiada@gmail.com>
2023-06-19 13:27:13 -0700

    Updated

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Structure datatype for dogs
+ *@name: Name of the dog
+ *@age: Age of the dog
+ *@owner: Owner of dog
+ *
+ *Description: Storing information of dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dogs_t - Typedef for dogs struct
+ */
+typedef struct dog dogs_t;
+
+#endif
