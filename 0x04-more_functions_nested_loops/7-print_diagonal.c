758218dbde77b30a5b4c4d80d28f4227e74f59b6
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-01 06:34:10 -0700

    New Update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,16 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar('\n');
 	else
+	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 1; j < i; j++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
 			_putchar ('\n');
 		}
+	}
 
 }
