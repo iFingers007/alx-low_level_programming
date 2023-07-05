e7b31ce966322c5d4bf4130ff14a919277254d9c
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-05 09:15:58 -0700

    Updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,13 +15,12 @@ void free_listint2(listint_t **head)
 	listint_t *p;
 
 	if (*head == NULL)
-		return;
-	p = *head;
+		free(*head);
 	while (*head != NULL)
 	{
+		p = *head;
 		*head = (*head)->next;
 		free(p);
 	}
 	*head = NULL;
-	free(p);
 }
