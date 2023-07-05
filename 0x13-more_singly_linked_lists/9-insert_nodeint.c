3a435cd474a5559c49491a9815e4f68c587f94e3
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-05 14:30:16 -0700

    Updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,13 +36,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev_node = temp;
 		temp = temp->next;
 	}
-	if (idx < 0)
-	{
-		free(newnode);
-		return (NULL);
-	}
 	newnode->next = temp->next;
-	temp->next = newnode;
+	prev_node->next = newnode;
 
 	return (newnode);
 }
