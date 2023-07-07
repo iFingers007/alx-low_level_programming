455a38f5d129ce62b2729289863d582a1273bf49
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-07 12:25:25 -0700

    Updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,17 +12,7 @@
 
 void print_binary(unsigned long int n)
 {
-
-	unsigned long int ex = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	int pos = sizeof(unsigned long int) * 8;
-
-	while (pos > 0)
-	{
-		if (n & ex)
-			_putchar('1');
-		else
-			_putchar('0');
-		ex >>= 1;
-		pos--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
