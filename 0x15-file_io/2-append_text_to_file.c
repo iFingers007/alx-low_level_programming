73d138fad2d16cfcd685fa900a195b78add6acdf
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-11 08:46:22 -0700

    Updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fl = open(filename, O_WRONLY | O_APPEND);
 	if (fl == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -34,8 +34,6 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	else
-		return (-1);
 	close(fl);
 	return (1);
 }
