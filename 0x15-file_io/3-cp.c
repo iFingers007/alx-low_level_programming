f04ae9df8791822f2171a09e8e4237f02f41c077
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-11 13:20:01 -0700

    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,25 +17,23 @@ int main(int argc, char *argv[])
 {
 	int ff, ft, cff, cft;
 	char buffer[1024];
-	char *file_from = argv[1];
-	char *file_to = argv[2];
-	size_t bw, br;
+	int bw, br;
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	ft = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 00664);
-	ff = open(file_from, O_RDWR);
+	ft = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	ff = open(argv[1], O_RDWR);
 	br = read(ff, &buffer, 1024);
-	if (ff == -1 || br == 0)
+	if (ff == -1 || br == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	bw = write(ft, &buffer, br);
-	if (bw == 0 || ft == -1)
+	if (ft == -1 || bw == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
