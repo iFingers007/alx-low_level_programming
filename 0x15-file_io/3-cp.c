dd0d08a7544ec9cf99bb97ac47639c1b3bb13cbb
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-12 04:03:56 -0700

    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,29 +17,39 @@ int main(int argc, char *argv[])
 {
 	int ff, ft, cff, cft;
 	char buffer[1024];
-	int bw, br;
+	ssize_t bw, br;
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
-	ft = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0004 | 0400 | 0200 | 0040 | 0020);
-	ff = open(argv[1], O_RDONLY);
-	br = read(ff, &buffer, 1024);
-	if (ff == -1 || br == -1)
+	ft = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0004 | 0400 | 0200 | 0040 | S_IWGRP);
+	if (ft == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	bw = write(ft, &buffer, br);
-	if (ft == -1 || bw == -1)
+	ff = open(argv[1], O_RDONLY);
+	if (ff == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
+	while ((br = read(ff, &buffer, 1024)) > 0)
+	{
+		bw = write(ft, &buffer, br);
+			if (bw == -1)
+			{
+				dprintf(2, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+	}
+	if (br == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	cff = close(ff);
 	if (cff == -1)
 	{
