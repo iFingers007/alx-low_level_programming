7b1fec244398762d0acdafba6e53dc89257d5692
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-12 06:09:20 -0700

    New Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,62 +5,93 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <stdarg.h>
+/**
+ *perror_exit - For printing the error messages and exit
+ *@msg: Error message to be printed
+ *@exn: Exit Number
+ *
+ *Return: Void
+*/
+
+void perror_exit(char *msg, int exn, ...)
+{
+	unsigned int i = 0;
+	va_list list;
+	char *value;
+
+	va_start(list, exn);
+	while (msg[i] != '\0')
+	{
+		if (msg[i] == '%')
+		{
+			value = va_arg(list, char *);
+			dprintf(STDERR_FILENO, "%s ", value);
+			i++;
+		}
+		dprintf(STDERR_FILENO, "%s\n", msg);
+		exit(exn);
+	}
+	va_end(list);
+}
+
+/**
+ *_cp - Copies a file
+ *@src: File to copy from
+ *@dst: File to copy to
+ *
+ *Return: Void
+*/
+
+void _cp(char *src, char *dst)
+{
+	int ff, ft, cff, cft;
+	char buffer[1024];
+	ssize_t br, bw;
+
+	ff = open(src, O_RDONLY);
+	if (ff == -1)
+		perror_exit("Error: Can't read from file %s\n", 98, dst);
+	ft = open(dst, O_WRONLY | O_CREAT | O_TRUNC,
+	  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (ft == -1)
+		perror_exit("Error: Can't write to %s\n", 99, dst);
+	while ((br = read(ff, &buffer, 1024)) > 0)
+	{
+		bw = write(ft, &buffer, br);
+		if (bw == -1)
+			perror_exit("Error: Can't write to %s\n", 99, dst);
+
+	}
+	if (br == -1)
+		perror_exit("Error: Can't read from file %s\n", 98, dst);
+	cff = close(ff);
+	if (cff == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
+		exit(100);
+	}
+	cft = close(ft);
+	if (cft == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
+		exit(100);
+	}
+}
 
 /**
  *main - Entry to program
- *@:
+ *@argc: Command line argument count
+ *@argv: Argument Vector
  *
  *Return: 0 on success
 */
 
 int main(int argc, char *argv[])
 {
-	int ff, ft, cff, cft;
-	char buffer[1024];
-	ssize_t bw, br;
-
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-	ft = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0004 | 0400 | 0200 | 0040 | S_IWGRP);
-	if (ft == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	ff = open(argv[1], O_RDONLY);
-	if (ff == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((br = read(ff, &buffer, 1024)) > 0)
-	{
-		bw = write(ft, &buffer, br);
-			if (bw == -1)
-			{
-				dprintf(2, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-	}
-	if (br == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	cff = close(ff);
-	if (cff == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", ff);
-		exit(100);
-	}
-	cft = close(ft);
-	if (cft == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", ft);
-		exit(100);
-	}
+		perror_exit("Usage: %s file_from file_to\n", 97, argv[0]);
+	_cp(argv[1], argv[2]);
+
 	return (0);
 }
