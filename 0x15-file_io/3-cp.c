ccf0cb4b2a41a380d63a9e3f9c69e261ef626c67
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-13 14:48:16 -0700

    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <stdarg.h>
 #include <errno.h>
 
 /**
- *perror_exit - For printing the error messages and exit
- *@msg: Error message to be printed
- *@exn: Exit Number
+ *_close - Closes a file
+ *@_file: file to be closed
  *
- *Return: Void
+ *Return: 0 on success
 */
 
-void perror_exit(char *msg, int exn, ...)
+int _close(int _file)
 {
-	unsigned int i = 0;
-	va_list list;
-	char *value;
-
-	va_start(list, exn);
-	while (msg[i] != '\0')
+	int c;
+	c = close(_file);
+	if (c == -1)
 	{
-		if (msg[i] == '%')
-		{
-			value = va_arg(list, char *);
-			dprintf(STDERR_FILENO, "%s ", value);
-			i++;
-		}
-		dprintf(STDERR_FILENO, "%c\n", msg[i++]);
-		exit(exn);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _file);
+		return (100);
 	}
-	va_end(list);
+	return (0);
 }
 
 /**
@@ -42,58 +30,65 @@ void perror_exit(char *msg, int exn, ...)
  *@src: File to copy from
  *@dst: File to copy to
  *
- *Return: Void
+ *Return: 0 on success
 */
-
-void _cp(char *src, char *dst)
+int _cp(char *src, char *dst)
 {
-	int ff, ft, cff, cft;
+	int ff, ft, br, bw;
 	char buffer[1024];
-	ssize_t br, bw;
 
 	ff = open(src, O_RDONLY);
 	if (ff == -1)
-		perror_exit("Error: Can't read from file %s\n", 98, src);
-	ft = open(dst, O_WRONLY | O_CREAT | O_TRUNC,
-	  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		close(ff);
+		return (98);
+	}
+	ft = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (ft == -1)
-		perror_exit("Error: Can't write to %s\n", 99, dst);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
+		close(ft);
+		return (99);
+	}
 	while ((br = read(ff, &buffer, 1024)) > 0)
 	{
 		bw = write(ft, &buffer, br);
 		if (bw == -1)
-			perror_exit("Error: Can't write to %s\n", 99, dst);
-
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
+			close(ff);
+			close(ft);
+			return (99);
+		}
 	}
 	if (br == -1)
-		perror_exit("Error: Can't read from file %s\n", 98, src);
-	cff = close(ff);
-	if (cff == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		close(ff);
+		close(ft);
+		return (98);
 	}
-	cft = close(ft);
-	if (cft == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
-		exit(100);
-	}
+	_close(ft);
+	_close(ft);
+	return (0);
 }
-
 /**
  *main - Entry to program
- *@argc: Command line argument count
- *@argv: Argument Vector
+ *@argc: Argument count
+ *@argv: Aegument vector
  *
  *Return: 0 on success
 */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
-		perror_exit("Usage: %s file_from file_to\n", 97, argv[0]);
-	_cp(argv[1], argv[2]);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		return (97);
+	}
+	if (_cp(argv[1], argv[2]) != 0)
+		return (1);
 
 	return (0);
 }
