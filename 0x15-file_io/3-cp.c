82a2dbd622344f42c8ce5910aafaf4abf9dd04fd
Abu Jehoiada <abujehoiada@gmail.com>
2023-07-13 15:13:50 -0700

    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,14 +42,12 @@ int _cp(char *src, char *dst)
 	if (ff == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		close(ff);
 		return (98);
 	}
 	ft = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (ft == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
-		close(ft);
 		return (99);
 	}
 	while ((br = read(ff, &buffer, 1024)) > 0)
@@ -58,19 +56,15 @@ int _cp(char *src, char *dst)
 		if (bw == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dst);
-			close(ff);
-			close(ft);
 			return (99);
 		}
 	}
 	if (br == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
-		close(ff);
-		close(ft);
 		return (98);
 	}
-	_close(ft);
+	_close(ff);
 	_close(ft);
 	return (0);
 }
@@ -88,8 +82,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		return (97);
 	}
-	if (_cp(argv[1], argv[2]) != 0)
-		return (1);
+	_cp(argv[1], argv[2]);
 
 	return (0);
 }
