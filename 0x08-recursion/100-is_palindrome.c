f9a8797ac3101d60544cb7de3194a8cc061c8675
Abu Jehoiada <abujehoiada@gmail.com>
2023-08-31 07:40:57 -0700

    Code update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,17 +18,19 @@ int _strlen_recursion(char *s)
 /**
  * chck_palindrome - Checks for palindrome
  *@s: String to be checked
- *
+ *@fn: First index
+ *@ln: Last index
  *Return: 1 if it is and zero if it's not
  */
 
-int chck_palindrome(char *s)
+int chck_palindrome(char *s, int fn, int ln)
 {
-	len = _strlen_recursion(s);
-	if (*s = "")
+	if (fn == ln)
 		return (1);
+	if (s[fn] != s[ln])
+		return (0);
 	else
-		chck_palindrome
+		return (chck_palindrome(s, s[fn + 1], s[ln - 1]));
 }
 
 /**
@@ -40,19 +42,10 @@ int chck_palindrome(char *s)
 
 int is_palindrome(char *s)
 {
-	if 
-}
-/**
- * _strlen_recursion - Gets length of a string using recursion
- *@s: String input
- *
- *Return: Length of the string
- */
+	int len;
 
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	len = _strlen_recursion(s);
+	if (len == 0)
+		return (1);
+	return (chck_palindrome(s, 0, len - 1));
 }
