fffdc01a4927b09f3df6d5db34195ab07e20435e
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-05 06:24:12 -0700

    Code update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings
+ *@s1: Destination string
+ *@s2: Source string
+ *Return: Memory address allocated or Null if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int len1, len2, i, j;
+
+	if (s2 == NULL)
+		return ("");
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	p = malloc(sizeof(char) * ((len1 + len2) + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (i = len1, j = 0; s2[j] != '\0'; i++, j++)
+		p[i] = s2[j];
+	p[i] = '\0';
+	return (p);
+}
+
+/**
+ * _strlen - Gets length of a string
+ *@str: String to find length
+ *
+ *Return: Length of string
+ */
+
+int _strlen(char *str)
+{
+	unsigned int l = 0;
+
+	if (str == NULL)
+		return (0);
+	while (*str != '\0')
+	{
+		l++;
+		str++;
+	}
+	return (l);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
 int _strlen(char *str);
 char *create_array(unsigned int size, char c);
