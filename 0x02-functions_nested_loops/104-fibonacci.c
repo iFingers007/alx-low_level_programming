7dc9b8107c8ee4d56e30c23e11d8208b0fef4939
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-12 09:21:47 -0700

    104-fibonacci edited

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,20 @@
 
 int main(void)
 {
-	unsigned long fn = 1, sn = 2, temp = 0, i;
+	unsigned long fn = 1, sn = 2, temp = 0, i = 2;
 
 	printf("%lu, %lu", fn, sn);
-	for (i = 1; i < 96; i++)
+	for (; i < 98; i++)
 	{
-		temp = fn;
+		temp = fn + sn;
 		fn = sn;
-		sn = temp + fn;
-		printf(", %lu", sn);
+		b = temp;
+		printf("%lu", sn);
+		if (i < 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 
 	return (0);
 
