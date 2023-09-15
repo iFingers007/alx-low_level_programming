def0c3581697c9e9d6ebb714332bbc9ac9703a8b
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-15 13:30:22 -0700

    Code update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @p: The specifier
+ * @f: The function associated
+ */
+typedef struct pr
+{
+	char *p;
+	void (*f)(va_list);
+} pr_t;
+
+/*function prototypes*/
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
