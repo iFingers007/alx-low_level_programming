af09080795657d5fe88e7d4b88a01194635d16de
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-21 14:58:16 -0700

    Code update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+void premain (void) __attribute__((constructor));
+/**
+ *  -
+ *@:
+ *
+ *Return:
+ */
+
+void premain (void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
