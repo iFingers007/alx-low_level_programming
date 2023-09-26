28245fe7f44fcf6582b68944b3fb28202c0bb344
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-26 07:11:33 -0700

    Code update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	temp = head;
-	while (i <= index)
+	while (i < index)
 	{
 		temp = temp->next;
 		i++;
