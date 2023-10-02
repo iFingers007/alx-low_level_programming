33c6e1c5efe76b8ca9067796fa3a40f9860fdfdc
Abu Jehoiada <abujehoiada@gmail.com>
2023-10-02 08:34:13 -0700

    Code update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,42 +9,24 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t chars_r, chars_w;
-	int fd;
+	ssize_t r, w, o;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(2, "Fd Error:");
-		return (0);
-	}
-	buf = malloc(sizeof(char) * (letters + 1));
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
-	{
-		dprintf(2, "Malloc Error:");
 		return (0);
-	}
-	chars_r = read(fd, buf, letters);
-	if (chars_r == -1)
-	{
-		dprintf(2, "Read Error:");
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	buf[chars_r] = '\0';
-	chars_w = write(STDOUT_FILENO, buf, chars_r);
-	if (chars_w != chars_r)
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || o == -1 || r == -1 || w != r)
 	{
 		dprintf(2, "Write Error:");
 		free(buf);
-		close(fd);
 		return (0);
 	}
 	free(buf);
-	close(fd);
-	return (chars_w);
+	close(o);
+	return (w);
 }
