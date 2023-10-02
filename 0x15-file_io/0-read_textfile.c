8c3b623a8dd0c11c5ba992525957cca7b59d894c
Abu Jehoiada <abujehoiada@gmail.com>
2023-10-02 08:39:36 -0700

    Code update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,9 +20,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 	r = read(o, buf, letters);
 	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1 || o == -1 || r == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		dprintf(2, "Write Error:");
 		free(buf);
 		return (0);
 	}
