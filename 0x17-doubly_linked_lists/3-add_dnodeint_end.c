7765e7bbb984c48471f96c52a6b023e9a56a63e0
Abu Jehoiada <abujehoiada@gmail.com>
2024-03-21 06:29:32 -0700

    Update

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (temp->next != NULL)
 		{
-			temp = temp  ->next;
+			temp = temp->next;
 		}
 		temp->next = new;
 		new->prev = temp;
