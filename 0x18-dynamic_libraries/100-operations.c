bab31f8afa9424bc78f92d24063f942c694e56ab
Abu Jehoiada <abujehoiada@gmail.com>
2024-03-25 06:42:32 -0700

    Update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Introduction to program
+ *
+ * Return: 1 or 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+        return (a - b);
+}
+int mul(int a, int b)
+{
+        return (a * b);
+}
+int div (int a, int b)
+{
+        return (a / b);
+}
+int mod (int a, int b)
+{
+        return (a % b);
+}
